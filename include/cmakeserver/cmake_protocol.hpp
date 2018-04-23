bc3d596e84094d697d55923365bc9d7e4c22aadd
Morris Hafner <morris.hafner@codeplay.com>
2018-04-23 22:38:53 +0100

    CMake 3.11 fix, extended protocol definition

--- include/cmakeserver/cmake_protocol.hpp
+++ include/cmakeserver/cmake_protocol.hpp
@@ -3,25 +3,100 @@
 #include <string>
 #include <string_view>
 #include <unordered_map>
+#include <optional>
 #include <variant>
 #include <vector>
+#include <experimental/filesystem>
+
+#define CMAKESERVER_PROTOCOL_MSG(name, ...) \
+    struct name : message { \
+        BOOST_HANA_DEFINE_STRUCT(name, \
+            (opt<std::string>, cookie), \
+            __VA_ARGS__ \
+        ); \
+    };
+#define CMAKESERVER_PROTOCOL_REPLY(name, ...) \
+    struct name : reply { \
+        BOOST_HANA_DEFINE_STRUCT(name, \
+            __VA_ARGS__ \
+        ); \
+    };
+#define CMAKESERVER_PROTOCOL_ERROR(name, ...) \
+    struct name : error { \
+        BOOST_HANA_DEFINE_STRUCT(name, \
+            __VA_ARGS__ \
+        ); \
+    };
 
 namespace cmakeserver::protocol {
-	inline namespace v3_11 {
+	inline namespace v1_2 {
+        template<typename T>
+        using opt = std::optional<T>;
+
 		auto constexpr prologue = std::string_view{"[== \"CMake Server\" ==[\n"};
 		auto constexpr epilogue = std::string_view{"]== \"CMake Server\" ==]\n"};
+
+        struct message {
+            BOOST_HANA_DEFINE_STRUCT(message,
+                (opt<std::string>, cookie)
+            );
+        };
 
 		// clang-format off
         struct hello {
             struct version {
                 BOOST_HANA_DEFINE_STRUCT(version,
                     (int, major),
-                    (int, minor)
+                    (int, minor),
+                    (opt<bool>, isExperimental)
                 );
             };
             BOOST_HANA_DEFINE_STRUCT(hello,
                 (std::vector<version>, supportedProtocolVersions)
             );
+        };
+
+        CMAKESERVER_PROTOCOL_MSG(handshake,
+                (opt<std::experimental::filesystem::path>, sourceDirectory),
+                (std::experimental::filesystem::path, buildDirectory),
+                (opt<std::string>, generator),
+                (opt<std::string>, extraGenerator),
+                (opt<std::string>, platform),
+                (opt<std::string>, toolset)
+            );
+
+        struct globalSettings{
+            BOOST_HANA_DEFINE_STRUCT(globalSettings);
+        };
+
+        struct configure {
+            BOOST_HANA_DEFINE_STRUCT(configure,
+                (std::vector<std::string>, cacheArguments)
+            );
+        };
+
+        struct compute {
+            BOOST_HANA_DEFINE_STRUCT(compute);
+        };
+
+        struct codemodel {
+            BOOST_HANA_DEFINE_STRUCT(codemodel);
+        };
+
+        struct ctestinfo {
+            BOOST_HANA_DEFINE_STRUCT(codemodel);
+        };
+
+        struct cmakeinputs {
+            BOOST_HANA_DEFINE_STRUCT(cmakeinputs);
+        };
+
+        struct cache {
+            BOOST_HANA_DEFINE_STRUCT(cache);
+        };
+
+        struct fileSystemWatchers {
+            BOOST_HANA_DEFINE_STRUCT(cache);
         };
 
         struct reply {
@@ -45,13 +120,6 @@ namespace cmakeserver::protocol {
                 (int, progressMinimum),
                 (int, progressMaximum),
                 (int, progressCurrent)
-            );
-        };
-
-        struct message {
-            BOOST_HANA_DEFINE_STRUCT(message,
-                (std::string, message),
-                (std::string, title)
             );
         };
 
